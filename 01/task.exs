b3d878214ed997a1f323973843192602c7d11747
Felix Koch <dev@felixkoch.at>
2025-12-13 20:59:53 +0100

    task01 part 2

--- 01/task.exs
+++ 01/task.exs
@@ -28,6 +28,32 @@ defmodule Task01 do
     end
   end
 
+  def rotate_and_count(amount, %{:pos => position}) do
+    rotate_and_count(position, amount, 0)
+  end
+
+  def rotate_and_count(amount, position) when is_number(position) do
+    rotate_and_count(position, amount, 0)
+  end
+
+  def rotate_and_count(position, amount, n_zeros)  do 
+    new = position + amount
+
+    cond do
+      new < 0 -> rotate_and_count(100, new, n_zeros + (if position != 0, do: 1, else: 0))
+      new == 100 -> %{pos: 0, n_zeros: n_zeros + 1}
+      new > 99 -> rotate_and_count(0, new - 100, n_zeros + 1)
+      new == 0 -> %{pos: new, n_zeros: n_zeros + 1}
+      true -> %{pos: new, n_zeros: n_zeros}
+    end
+  end
+
+  def total_zero_count(filename) do
+    raw_rotations(filename) |> Stream.scan(%{pos: 50, n_zeros: 0}, &rotate_and_count/2) |> dbg() |> Enum.reduce(
+      0, fn %{n_zeros: n}, acc -> acc + n end 
+    )
+  end
+
   def get_password(filename) do
     positions(filename) |> Enum.count(&(&1 == 0))
   end
